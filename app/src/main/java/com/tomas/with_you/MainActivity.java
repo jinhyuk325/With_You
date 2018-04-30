c6cedf769f342e9ea2e8c2888d2f07273fcc4d38
jin hyuk <jinhyuk325@nate.com>
2018-04-30 16:40:49 +0900

    third Commit

--- app/src/main/java/com/tomas/with_you/MainActivity.java
+++ app/src/main/java/com/tomas/with_you/MainActivity.java
@@ -1,24 +1,17 @@
 package com.tomas.with_you;
 
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.sql.Time;
-import java.text.DateFormat;
+import java.text.Collator;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
@@ -26,15 +19,14 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     TabHost tabHost;
-    ArrayList<String> datas = new ArrayList<>();
+    ArrayList<Member> members = new ArrayList<>();
     ListView listView;
-    ArrayAdapter adapter;
+//    ArrayAdapter adapter;
+    MyAdapter adapter;
 
 
 
-    ListView listView2;
-    ArrayList<String> datas2 = new ArrayList<>();
-    ArrayAdapter adapter2;
+
 
 
     @Override
@@ -46,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
         //탭버튼(TabSpec)을 생성해서 TabHost에 추가
         tabHost.setup();//탭호스트에게 탭위젯의 버튼이 변경(생성)된다는 것을 알리는 목적.
 
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("달력").setContent(R.id.tab1));
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("100일단위\n기념일").setContent(R.id.tab2));
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("1년단위\n기념일").setContent(R.id.tab3));
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Main").setContent(R.id.tab1));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("기념일").setContent(R.id.tab2));
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("설정").setContent(R.id.tab3));
 
 
 //        String day = DateFormat.getDateInstance().format(new Date());
@@ -58,17 +50,16 @@ public class MainActivity extends AppCompatActivity {
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 //        String getTime = sdf.format(date);
 
-        int Y;
-        int M;
-        int D;
+//        int Y;
+//        int M;
+//        int D;
+//
+//        // 현재 날짜 구하기
+//        Calendar calendar = new GregorianCalendar(Locale.KOREA);
+//        Y = calendar.get(Calendar.YEAR);
+//        M = calendar.get(Calendar.MONTH) + 1;
+//        D = calendar.get(Calendar.DAY_OF_MONTH);
 
-        // 현재 날짜 구하기
-        Calendar calendar = new GregorianCalendar(Locale.KOREA);
-        Y = calendar.get(Calendar.YEAR);
-        M = calendar.get(Calendar.MONTH) + 1;
-        D = calendar.get(Calendar.DAY_OF_MONTH);
-        System.out.println("GregorianCalendar = " + Y + "-"
-                + M  + "-" + D);
 
         // Date 로 구하기
         SimpleDateFormat fm1 = new SimpleDateFormat("yyyy년MM월dd일");
@@ -76,31 +67,39 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
         listView = findViewById(R.id.listview);
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, datas);
+//        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, members);
+
+        adapter=new MyAdapter(members,getLayoutInflater());
         listView.setAdapter(adapter);
-        datas.add("1일\n" + date);
+
+
+
         for (int i = 0; i < 100; i++) {
 
             Calendar cal = new GregorianCalendar(Locale.KOREA);
             cal.setTime(new Date());
 //        cal.add(Calendar.YEAR, 1); // 1년을 더한다.
 //        cal.add(Calendar.MONTH, 1); // 한달을 더한다.
-            cal.add(Calendar.DAY_OF_YEAR, (i+1)*100); // 하루를 더한다.
-            if(i==0){
-                cal.add(Calendar.DAY_OF_YEAR, -1);
-            }
+            cal.add(Calendar.DAY_OF_YEAR, (i+1)*100-1); // 하루를 더한다.
+
+
 //        cal.add(Calendar.HOUR, 1); // 시간을 더한다
 
             SimpleDateFormat fm = new SimpleDateFormat("yyyy년MM월dd일");
             String strDate = fm.format(cal.getTime());
 
-            datas.add((i + 1) + "00일\n" +strDate);
+            Date Date1=fm.parse(strDate, new ParsePosition(0));
+            Date Date2=fm1.parse(date, new ParsePosition(0));
+
+            long diff=Date1.getTime()-Date2.getTime();
+            String s= String.valueOf("D-"+diff/1000/60/24/60+"일");
+
+
+            members.add(new Member((i+1)+"00일",strDate,s));
         }
 
-        listView2 = findViewById(R.id.listview2);
-        adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, datas2);
-        listView2.setAdapter(adapter2);
         for (int i = 0; i < 30; i++) {
 
             Calendar cal = new GregorianCalendar(Locale.KOREA);
@@ -113,8 +112,32 @@ public class MainActivity extends AppCompatActivity {
             SimpleDateFormat fm = new SimpleDateFormat("yyyy년MM월dd일");
             String strDate = fm.format(cal.getTime());
 
-            datas2.add((i + 1) + "주년\n" +strDate);
+            Date Date1=fm.parse(strDate, new ParsePosition(0));
+            Date Date2=fm.parse(date, new ParsePosition(0));
+
+            long diff=Date1.getTime()-Date2.getTime();
+            String s= String.valueOf("D-"+diff/1000/60/24/60+"일");
+
+            members.add(new Member((i+1)+"주년",strDate,s));
         }
+
+
+
+
+
+//        for (int i = 0; i < 30; i++) {
+//        d_day.add(Calendar.YEAR, 1); // 1년을 더한다.
+////        cal.add(Calendar.MONTH, 1); // 한달을 더한다.
+////            cal.add(Calendar.DAY_OF_YEAR, (i+1)*365);
+////        cal.add(Calendar.HOUR, 1); // 시간을 더한다
+//
+//            SimpleDateFormat fm = new SimpleDateFormat("yyyy년MM월dd일");
+//            String strDate = fm.format(d_day.getTime());
+//
+
+//        }
+
+
 
 
     }
