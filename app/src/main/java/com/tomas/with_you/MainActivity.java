a0568e01baa87787cf9033e6581dbc2fd8475fa8
jin hyuk <jinhyuk325@nate.com>
2018-04-30 11:15:14 +0900

    third Commit

--- app/src/main/java/com/tomas/with_you/MainActivity.java
+++ app/src/main/java/com/tomas/with_you/MainActivity.java
@@ -20,14 +20,21 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     TabHost tabHost;
-    ArrayList<String> datas= new ArrayList<>();
-
+    ArrayList<String> datas = new ArrayList<>();
     ListView listView;
     ArrayAdapter adapter;
+
+
+
+    ListView listView2;
+    ArrayList<String> datas2 = new ArrayList<>();
+    ArrayAdapter adapter2;
 
 
     @Override
@@ -39,49 +46,78 @@ public class MainActivity extends AppCompatActivity {
         //탭버튼(TabSpec)을 생성해서 TabHost에 추가
         tabHost.setup();//탭호스트에게 탭위젯의 버튼이 변경(생성)된다는 것을 알리는 목적.
 
-        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("탭1").setContent(R.id.tab1));
-        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("탭2").setContent(R.id.tab2));
-        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("탭3").setContent(R.id.tab3));
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("달력").setContent(R.id.tab1));
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("100일단위\n기념일").setContent(R.id.tab2));
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("1년단위\n기념일").setContent(R.id.tab3));
 
 
 //        String day = DateFormat.getDateInstance().format(new Date());
 
-        long day=System.currentTimeMillis();
-        Date date=new Date(day);
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-        String getTime=sdf.format(date);
+//        long day = System.currentTimeMillis();
+//        Date date = new Date(day);
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        String getTime = sdf.format(date);
+
+        int Y;
+        int M;
+        int D;
+
+        // 현재 날짜 구하기
+        Calendar calendar = new GregorianCalendar(Locale.KOREA);
+        Y = calendar.get(Calendar.YEAR);
+        M = calendar.get(Calendar.MONTH) + 1;
+        D = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println("GregorianCalendar = " + Y + "-"
+                + M  + "-" + D);
+
+        // Date 로 구하기
+        SimpleDateFormat fm1 = new SimpleDateFormat("yyyy년MM월dd일");
+        String date = fm1.format(new Date());
 
 
 
-        listView= findViewById(R.id.listview);
-        adapter= new ArrayAdapter(this, android.R.layout.simple_list_item_1, datas);
+        listView = findViewById(R.id.listview);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, datas);
         listView.setAdapter(adapter);
-        datas.add("1일\n"+getTime);
-        for(int i=0;i<100;i++){
-            datas.add((i+1)+"00일\n");
+        datas.add("1일\n" + date);
+        for (int i = 0; i < 100; i++) {
+
+            Calendar cal = new GregorianCalendar(Locale.KOREA);
+            cal.setTime(new Date());
+//        cal.add(Calendar.YEAR, 1); // 1년을 더한다.
+//        cal.add(Calendar.MONTH, 1); // 한달을 더한다.
+            cal.add(Calendar.DAY_OF_YEAR, (i+1)*100); // 하루를 더한다.
+            if(i==0){
+                cal.add(Calendar.DAY_OF_YEAR, -1);
+            }
+//        cal.add(Calendar.HOUR, 1); // 시간을 더한다
+
+            SimpleDateFormat fm = new SimpleDateFormat("yyyy년MM월dd일");
+            String strDate = fm.format(cal.getTime());
+
+            datas.add((i + 1) + "00일\n" +strDate);
         }
 
+        listView2 = findViewById(R.id.listview2);
+        adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, datas2);
+        listView2.setAdapter(adapter2);
+        for (int i = 0; i < 30; i++) {
 
+            Calendar cal = new GregorianCalendar(Locale.KOREA);
+            cal.setTime(new Date());
+//        cal.add(Calendar.YEAR, 1); // 1년을 더한다.
+//        cal.add(Calendar.MONTH, 1); // 한달을 더한다.
+            cal.add(Calendar.DAY_OF_YEAR, (i+1)*365);
+//        cal.add(Calendar.HOUR, 1); // 시간을 더한다
+
+            SimpleDateFormat fm = new SimpleDateFormat("yyyy년MM월dd일");
+            String strDate = fm.format(cal.getTime());
+
+            datas2.add((i + 1) + "주년\n" +strDate);
+        }
 
 
     }
-    public class dday{ public int caldate(int myear, int mmonth, int mday) {
-        try {
 
-            Calendar today = Calendar.getInstance(); //현재 오늘 날짜
-            Calendar dday = Calendar.getInstance();
-            dday.set(myear,mmonth,mday);// D-day의 날짜를 입력합니다.
-            long day = dday.getTimeInMillis()/86400000;
-            long tday = today.getTimeInMillis()/86400000;
-            long count = tday - day; // 오늘 날짜에서 dday 날짜를 빼주게 됩니다.
-            return (int) count+1; // 날짜는 하루 + 시켜줘야합니다.
-        }
 
-        catch (Exception e)
-        {
-            e.printStackTrace(); return -1;
-        }
-
-        }
-    }
 }
